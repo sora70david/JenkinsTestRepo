6c8651c252d67f66d4d718a6888b0d42bfb037e2
David <15983017+sora70david@users.noreply.github.com>
2020-05-07 11:50:35 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
   public class HelloWorld{
     public static void main(String[] args){
-      System.out.println("------****** Hello World & welcome to Jenkins Training ******------");
+      System.out.println("------****** Made Some Changes Here ******------");
     }
   }
  
